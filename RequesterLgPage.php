b96e95bcb3638436a3c1eedf86d1656db4a3b39d
Syed Salman Sami <syedsamigithub@gmail.com>
2024-11-13 00:08:31 +0600

    RequesterLogin-Sami

---
+++ RequesterLgPage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- BTC -->
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+<!-- FA -->
+ <link rel="stylesheet" href="../css/all.min.css">
+ <!-- CusC -->
+  <link rel="stylesheet" href="../css/custom.css">
+
+
+</head>
+<body>
+    <!-- nav -->
+
+  <nav class="navbar navbar-dark fixed-top bg-info flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="RequesterLgPage.php">OSMS</a>
+ <!-- side bar start here -->
+<div class="container-fluid" style="margin-top: 40px;">
+    <div class="row">
+        <div class="col-sm-2 bg-light sidebar py-5">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a class="nav-link"
+                     href="RequesterLgPage.php"><i class="fas fa-user"></i>Profile</a></li>
+                     <li class="nav-item"><a class="nav-link"
+                     href="RequesterLgPage.php"><i class="fab fa-accessible-icon"></i>Submit Request</a></li>
+                     <li class="nav-item"><a class="nav-link"
+                     href="RequesterLgPage.php"><i class="fab fa-align-center"></i>Service Status</a></li>
+                     <li class="nav-item"><a class="nav-link"
+                     href="RequesterLgPage.php"><i class="fas fa-key"></i>Change Password</a></li>
+                     <li class="nav-item"><a class="nav-link"
+                     href="RequesterLgPage.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li></ul>
+                     
+                     
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+ <!-- side bar ends here -->
+
+
+ <script src="../js/jquery.js"></script>
+ <script src="../js/proper .js"></script>
+ <script src="../js/bootstrap.min.js"></script>
+ <script src="../js/all.min.js"></script>
+</body>
+</html>
